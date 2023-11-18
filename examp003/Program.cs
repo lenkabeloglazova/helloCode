f3a849f70354edd87d70004706a57da04f05d5f2
Elena <lenkabeloglazova@yandex.ru>
2023-11-18 14:32:46 +0300

    random

--- examp003/Program.cs
+++ examp003/Program.cs
@@ -1,3 +1,5 @@
-﻿int numA = 3;
-int numB = 5;
+﻿int numA = new Random().Next(1, 10);// 1 2 3 4..9
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
 Console.WriteLine(numA + numB);
